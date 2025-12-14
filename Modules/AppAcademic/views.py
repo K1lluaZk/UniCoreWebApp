d36bd0f29d9d490b71127c0d258077782e934ce3
Mario Suero <mario.suerodejesus@gmail.com>
2025-12-14 16:29:37 -0400

    Finish the contact view
    Finishing creating the contact view and scheduling message sending via form

--- Modules/AppAcademic/views.py
+++ Modules/AppAcademic/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
 def contactForm(request):
     return render(request, "contactform.html")
 
-def
+def contact(request):
+    if request.method == "POST":
+        asunto = request.POST.get("txtAsunto", "")
+        mensaje = request.POST.get("txtMensaje", "") + " / Email: " + request.POST.get("txtEmail", "")
+        email_desde = settings.EMAIL_HOST_USER
+        email_para = ["correopara"]
+        send_mail(asunto, mensaje, email_desde, email_para, fail_silently=False)
+        return render(request, "successFulContact.html")
+    return render(request, "contactForm.html")
+
+
+
--- UniCore/urls.py
+++ UniCore/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Modules.AppAcademic.views import contactForm
+from Modules.AppAcademic.views import contactForm, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contactForm/', contactForm),
+    path('contact/', contact)
 ]
