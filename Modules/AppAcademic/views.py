e878668d8e0c9b08c2007e9381332b12abefbb9f
Mario Suero <mario.suerodejesus@gmail.com>
2025-12-14 13:47:41 -0400

    Creating a contact form view

--- Modules/AppAcademic/views.py
+++ Modules/AppAcademic/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def contactForm(request):
+    return render(request, "contactform.html")
+
+def
--- UniCore/settings.py
+++ UniCore/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'UniCore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:/Users/mario/UniCoreWebApp/Modules/AppAcademic/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
