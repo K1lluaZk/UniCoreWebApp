c6c62693d06af9225983d4a29252cd6714497a4e
Mario Suero <mario.suerodejesus@gmail.com>
2025-12-20 19:19:23 -0400

    deleting non-functional things and details

--- Modules/AppAcademic/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("user/", views.user_dashboard, name="user_dashboard"),
-    path("user/careers/", views.user_career_list, name="user_career_list"),
-]
